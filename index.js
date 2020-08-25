18cf69ed94b6e286c6343dad2b7d8001ead24197
Harikrishnan <hkkrishn563@gmail.com>
2020-08-25 10:26:30 -0700

    MongoDB Connection

--- index.js
+++ index.js
@@ -4,8 +4,11 @@
 //Description: This is the entry file for our API Server
 const express = require('express');
 
+
+
 //initialize server
 const server = express();
+const config = require('./config/dev')
 //initialize entry point to the server
 server.get('/test',(req,res)=>{
     return res.json({message:"test is working"})
@@ -14,6 +17,23 @@ server.get('/test',(req,res)=>{
 //import portfolio routes
 const portfolioRoutes = require('./routes/portfolios')
 
+//Mongoose is a MongoDB object modeling tool designed to work in an
+//asynchronous environment. Mongoose supports both promises and callbacks.
+//import mongoose to link node server to MongoDB
+
+const mongoose = require('mongoose');
+//connection string gathered from mongoDB
+mongoose.connect(config.DB_URI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true,
+},(err)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log('Database successfully connected')
+    }
+})
 
 //employ route using a basepath
 server.use('/api/v1/portfolios',portfolioRoutes)
